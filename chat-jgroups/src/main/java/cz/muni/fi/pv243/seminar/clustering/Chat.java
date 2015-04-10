HEm3R/a4m36jee-2014-clustering-seminar

[clustering-seminar] Initial version of clustering seminar

chat-jgroups/src/main/java/cz/ctu/fee/a4m36jee/seminar/clustering/Chat.java
@@ -1,0 +1,55 @@
+package cz.ctu.fee.a4m36jee.seminar.clustering;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
+
+/**
+ * @author Radoslav Husar
+ */
+public class Chat extends ReceiverAdapter {
+
+    JChannel channel;
+    String user_name = System.getProperty("user.name", "n/a");
+    final List<String> state = new LinkedList<>();
+
+    @Override
+    public void viewAccepted(View new_view) {
+        // TODO: Someone joined the chat.
+    }
+
+    @Override
+    public void receive(Message msg) {
+        // TODO: New chat message received.
+    }
+
+    private void start() throws Exception {
+        channel = new JChannel("udp.xml");
+        channel.setReceiver(this);
+        channel.connect("ChatCluster");
+        channel.getState(null, 10000);
+        eventLoop();
+        channel.close();
+    }
+
+    /**
+     * This method reads input from user and sends over to other chat clients over the channel.
+     */
+    private void eventLoop() {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        for (;;) {
+            // TODO: Read text from user
+            // TODO: handle quit command
+            // TODO: Send message containing only the String object itself as payload
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Chat().start();
+    }
+}

remote-ejb/client-side/src/main/java/cz/ctu/fee/a4m36jee/seminar/clustering/ejb/remote/client/StatefulRemoteClient.java
@@ -1,0 +1,63 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.clustering.ejb.remote.client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import cz.ctu.fee.a4m36jee.seminar.clustering.ejb.remote.stateful.StatefulRemote;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public class StatefulRemoteClient {
+
+    public static void main(String[] args) throws Exception {
+        StatefulRemote statefulBean = lookupStatefulRemote();
+
+        String input = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // TODO: implement sending string to server and lookuping the list of them
+        // TODO: watch how the cluster behaves when one of the nodes goes down
+    }
+
+    /**
+     * Do remote lookup.
+     *
+     * @see https://docs.jboss.org/author/display/WFLY8/EJB+invocations+from+a+remote+client+using+JNDI
+     */
+    private static StatefulRemote lookupStatefulRemote() throws NamingException {
+        final Properties jndiProperties = new Properties();
+
+        // TODO: set up properties
+        final Context ctx = new InitialContext(jndiProperties);
+        // TODO: jndi lookup for stateful bean
+
+        return null;
+    }
+}

remote-ejb/server-side/src/main/java/cz/ctu/fee/a4m36jee/seminar/clustering/ejb/remote/stateful/StatefulBean.java
@@ -1,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.clustering.ejb.remote.stateful;
+
+import java.util.List;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public class StatefulBean implements StatefulRemote {
+
+    @Override
+    public void addString(String str) {
+        // TODO: implement
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> getStrings() {
+        // TODO: implement
+        throw new UnsupportedOperationException();
+    }
+}

remote-ejb/server-side/src/main/java/cz/ctu/fee/a4m36jee/seminar/clustering/ejb/remote/stateful/StatefulRemote.java
@@ -1,0 +1,40 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.clustering.ejb.remote.stateful;
+
+import java.util.List;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public interface StatefulRemote {
+
+    /**
+     * Adding string.
+     */
+    void addString(String str);
+
+    /**
+     * @return list of saved strings
+     */
+    List<String> getStrings();
+}

remote-ejb/server-side/src/main/java/cz/ctu/fee/a4m36jee/seminar/clustering/ejb/remote/stateless/StatelessBean.java
@@ -1,0 +1,34 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.clustering.ejb.remote.stateless;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public class StatelessBean implements StatelessRemote {
+
+    @Override
+    public String getNodeName() {
+        // TODO: implement
+        throw new UnsupportedOperationException();
+    }
+}

remote-ejb/server-side/src/main/java/cz/ctu/fee/a4m36jee/seminar/clustering/ejb/remote/stateless/StatelessRemote.java
@@ -1,0 +1,33 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.clustering.ejb.remote.stateless;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public interface StatelessRemote {
+
+    /**
+     * @return name of node where this method is called
+     */
+    String getNodeName();
+}

remote-ejb/server-side/src/main/java/cz/ctu/fee/a4m36jee/seminar/clustering/ejb/remote/util/NodeNameGetter.java
@@ -1,0 +1,39 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.clustering.ejb.remote.util;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public final class NodeNameGetter {
+
+    /**
+     * Returns name of the node where this is called.
+     */
+    public static String getNodeName() {
+        // TODO: Get property "jboss.node.name"
+        throw new UnsupportedOperationException();
+    }
+
+    private NodeNameGetter() {
+    }
+}

qa/pv243-2015-clustering-seminar

chat-jgroups/src/main/java/cz/muni/fi/pv243/seminar/clustering/Chat.java
@@ -1,0 +1,56 @@
+package cz.muni.fi.pv243.seminar.clustering;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
+
+/**
+ * @author Radoslav Husar
+ */
+public class Chat extends ReceiverAdapter {
+
+    JChannel channel;
+    String user_name = System.getProperty("user.name", "n/a");
+    final List<String> state = new LinkedList<>();
+
+    @Override
+    public void viewAccepted(View new_view) {
+        // TODO: Someone joined the chat.
+    }
+
+    @Override
+    public void receive(Message msg) {
+        // TODO: New chat message received.
+    }
+
+    private void start() throws Exception {
+        channel = new JChannel("udp.xml");
+        channel.setReceiver(this);
+        channel.connect("ChatCluster");
+        channel.getState(null, 10000);
+        eventLoop();
+        channel.close();
+    }
+
+    /**
+     * This method reads input from user and sends over to other chat clients over the channel.
+     */
+    private void eventLoop() {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        for (;;) {
+            // TODO: Read text from user
+            // TODO: handle quit command
+            // TODO: Send message containing only the String object itself as payload
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Chat().start();
+    }
+}

remote-ejb/client-side/src/main/java/cz/muni/fi/pv243/seminar/clustering/ejb/remote/client/StatefulRemoteClient.java
@@ -1,0 +1,63 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.clustering.ejb.remote.client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import cz.muni.fi.pv243.seminar.clustering.ejb.remote.stateful.StatefulRemote;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public class StatefulRemoteClient {
+
+    public static void main(String[] args) throws Exception {
+        StatefulRemote statefulBean = lookupStatefulRemote();
+
+        String input = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // TODO: implement sending string to server and lookuping the list of them
+        // TODO: watch how the cluster behaves when one of the nodes goes down
+    }
+
+    /**
+     * Do remote lookup.
+     *
+     * @see https://docs.jboss.org/author/display/WFLY8/EJB+invocations+from+a+remote+client+using+JNDI
+     */
+    private static StatefulRemote lookupStatefulRemote() throws NamingException {
+        final Properties jndiProperties = new Properties();
+
+        // TODO: set up properties
+        final Context ctx = new InitialContext(jndiProperties);
+        // TODO: jndi lookup for stateful bean
+
+        return null;
+    }
+}

remote-ejb/server-side/src/main/java/cz/muni/fi/pv243/seminar/clustering/ejb/remote/stateful/StatefulBean.java
@@ -1,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.clustering.ejb.remote.stateful;
+
+import java.util.List;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public class StatefulBean implements StatefulRemote {
+
+    @Override
+    public void addString(String str) {
+        // TODO: implement
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> getStrings() {
+        // TODO: implement
+        throw new UnsupportedOperationException();
+    }
+}

remote-ejb/server-side/src/main/java/cz/muni/fi/pv243/seminar/clustering/ejb/remote/stateful/StatefulRemote.java
@@ -1,0 +1,40 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.clustering.ejb.remote.stateful;
+
+import java.util.List;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public interface StatefulRemote {
+
+    /**
+     * Adding string.
+     */
+    void addString(String str);
+
+    /**
+     * @return list of saved strings
+     */
+    List<String> getStrings();
+}

remote-ejb/server-side/src/main/java/cz/muni/fi/pv243/seminar/clustering/ejb/remote/stateless/StatelessBean.java
@@ -1,0 +1,34 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.clustering.ejb.remote.stateless;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public class StatelessBean implements StatelessRemote {
+
+    @Override
+    public String getNodeName() {
+        // TODO: implement
+        throw new UnsupportedOperationException();
+    }
+}

remote-ejb/server-side/src/main/java/cz/muni/fi/pv243/seminar/clustering/ejb/remote/stateless/StatelessRemote.java
@@ -1,0 +1,33 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.clustering.ejb.remote.stateless;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public interface StatelessRemote {
+
+    /**
+     * @return name of node where this method is called
+     */
+    String getNodeName();
+}

remote-ejb/server-side/src/main/java/cz/muni/fi/pv243/seminar/clustering/ejb/remote/util/NodeNameGetter.java
@@ -1,0 +1,39 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.clustering.ejb.remote.util;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public final class NodeNameGetter {
+
+    /**
+     * Returns name of the node where this is called.
+     */
+    public static String getNodeName() {
+        // TODO: Get property "jboss.node.name"
+        throw new UnsupportedOperationException();
+    }
+
+    private NodeNameGetter() {
+    }
+}