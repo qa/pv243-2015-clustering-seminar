HEm3R/a4m36jee-2014-clustering-seminar

[clustering-seminar] Complete remote EJB demo

remote-ejb/server-side/src/main/java/cz/ctu/fee/a4m36jee/seminar/clustering/ejb/remote/stateless/StatelessBean.java
@@ -21,14 +21,18 @@
  */
 package cz.ctu.fee.a4m36jee.seminar.clustering.ejb.remote.stateless;
 
+import javax.ejb.Stateless;
+
+import cz.ctu.fee.a4m36jee.seminar.clustering.ejb.remote.util.NodeNameGetter;
+
 /**
  * @author Ondrej Chaloupka
  */
+@Stateless
 public class StatelessBean implements StatelessRemote {
 
     @Override
     public String getNodeName() {
-        // TODO: implement
-        throw new UnsupportedOperationException();
+        return NodeNameGetter.getNodeName();
     }
 }

qa/pv243-2015-clustering-seminar

remote-ejb/server-side/src/main/java/cz/muni/fi/pv243/seminar/clustering/ejb/remote/stateless/StatelessBean.java
@@ -21,14 +21,18 @@
  */
 package cz.muni.fi.pv243.seminar.clustering.ejb.remote.stateless;
 
+import javax.ejb.Stateless;
+
+import cz.muni.fi.pv243.seminar.clustering.ejb.remote.util.NodeNameGetter;
+
 /**
  * @author Ondrej Chaloupka
  */
+@Stateless
 public class StatelessBean implements StatelessRemote {
 
     @Override
     public String getNodeName() {
-        // TODO: implement
-        throw new UnsupportedOperationException();
+        return NodeNameGetter.getNodeName();
     }
 }